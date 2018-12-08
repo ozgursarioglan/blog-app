53819df6322d6829d37ce72a41429c612f385ec4
Özgür SARIOĞLAN <ozgursarioglan@hotmail.com>
2018-12-08 13:06:08 +0300

    Gerekli düzenlemeler yapıldı. Github yükleme için hazır.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+      resources :posts
+
+      root to: "users#index"
+    end
   resources :posts
   root to: 'posts#index'
   devise_for :users
